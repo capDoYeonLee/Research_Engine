d4c8cadcd36c0e71e7ba9853ff1dfae1d022e191
capDoYeonLee <ghtyu21@naver.com>
2023-02-18 21:44:23 +0900

    after chatGPT

--- search_app/views.py
+++ search_app/views.py
@@ -5,35 +5,41 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+#from setting_bulk import es
 from elasticsearch import Elasticsearch
 
 
 class SearchView(APIView):
 
     def get(self, request):
-        es = Elasticsearch([{'host':'localhost','port':'9200'}])
+        es = Elasticsearch([{'host':'localhost','port':'9200'}],timeout=30)
 
 
         # 검색어
-        search_word = request.GET.get('search')
+        search_word = request.query_params.get('search')
 
         if not search_word:
             return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': 'search word param is missing'})
+        print("docs start ============================="+search_word)
 
-        docs = es.search(index='dictionary',
-                         doc_type='dictionary_datas',
-                         body={
-                             "query": {
-                                 "multi_match": {
-                                     "query": search_word,
-                                     "fields": ["name", "about"]
-                                 }
-                             }
-                         })
+        try:
+            docs = es.search(index='dictionary',
+                             doc_type='dictionary_datas',
+                             body={
+                                 "query": {
+                                     "multi_match": {
+                                         "query": search_word,
+                                         "fields": ["name", "about"]
 
-        data_list = []
-        for data in docs['hits']['hits']:
-            data_list.append(data.get('_source'))
+                                     }
+                                 },
+                                 "_source": ["name", "about"]
+                             })
+            print(docs+"나지롱")
 
-        return Response(data_list)
+            data_list = docs['hits']['hits']
+            return Response(data_list)
+
+        except Exception as e:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={'message': f'Search failed: {e}'})
+
